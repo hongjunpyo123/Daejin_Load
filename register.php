2f87bc57b00c1e75616dda14b0bba6a3a6a00671
hongjunpyo <0721hjp@naver.com>
2024-05-13 13:20:41 +0900

    feat: 이번꺼 push하기 바랍니다

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ $email = $_POST['email'] ?? 0;
 $num = $_POST['num'] ?? 0;
 $password = $_POST['password'] ?? 0;
 $name = $_POST['name'] ?? 0;
-$today = date("Y/m/d") ?? 0; //컴퓨터의 현재 날짜 년/월/일 형식으로 읽어오기
+$today = date("Y/m/d") ?? 0; 
 
 
 $sql = "insert into userinfo values('$email','$num','$password','$name','$today')";
