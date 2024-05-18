6f3fb3c0a2798e1818481ac2fbb4c3ec5f767a6c
hongjunpyo <0721hjp@naver.com>
2024-05-18 22:23:34 +0900

    feat: 체육관, 여긱1 추가(building1.html, building2.html) - 사진은 추후에 추가 예정

--- main.php
+++ main.php
@@ -13,6 +13,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DAEJINLOAD</title>
+      <!-- Favicons -->
+  <link href="img/symbol.png" rel="icon">
 </head>
 <body>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -22,8 +24,14 @@
     <!--AOS라이브러리-->
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css"> 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> 
+    <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/jquery.fullpage.js"></script>
+        <link href="js/jquery.fullpage.css" rel="stylesheet">
     <style>
         html{
+            height: 100vh;
+        }
+        body{
             height: 100vh;
         }
         .navbar-brand{
@@ -55,7 +63,7 @@
         #sc{
             display: flex;
             flex-wrap: wrap;
-            height: 100vh;
+            height: 100%;
             justify-content: space-evenly;
             background-image: url("img/하늘.jpg");
             background-repeat: no-repeat;
@@ -108,8 +116,11 @@
             #low_bar{
                 opacity: 0;
             }
+            #number{
+            display: none;
         }
-        .first_card,.second_card,.third_card{
+        }
+        .first_card,.second_card,.third_card,.four_card,.five_card{
             text-align: center;
             background-color: white;
             border: 7px solid white;
@@ -119,7 +130,7 @@
             min-width: 220px;
             margin: 5px;
         }
-        .first_card:hover,.second_card:hover,.third_card:hover{
+        .first_card:hover,.second_card:hover,.third_card:hover,.four_card:hover,.five_card:hover{
             transition: box-shadow 0.3s ease-in-out;
             cursor: pointer;
             box-shadow: 4px 4px 4px gray;
@@ -139,11 +150,28 @@
             background-repeat: no-repeat;
             background-size: 100%;
         }
-        #first_tip,#second_tip,#third_tip{
+        .four_card{
+            background-image: url("img/학교지도.jpg");
+            background-repeat: no-repeat;
+            background-size: 100%;
+        }
+        .five_card{
+            background-image: url("img/채팅방.png");
+            background-repeat: no-repeat;
+            background-size: 100%;
+            background-position: center;
+        }
+        .btn-custom{
+            color: blue;
+        }
+        .btn-custom:hover{
+            background-color:cornflowerblue;
+        }
+        #first_tip,#second_tip,#third_tip,#four_tip,#five_tip{
             font-family: "Nanum Brush Script", cursive;
             font-weight: 1000;
             font-style: normal;
-            font-size: 20px;
+            font-size: 30px;
         }
         #number{
             transition: color 0.5s ease-in-out;
@@ -188,10 +216,11 @@
                   메뉴
                 </a>
                 <ul class="dropdown-menu">
-                <li ><a class="dropdown-item" href="#">수강신청 TIP</a></li>
+                <li ><a class="dropdown-item" href="ApplicationloginPage.html">수강신청 TIP</a></li>
                   <li><a class="dropdown-item" href="#">학교생활 TIP</a></li>
                   <li><a class="dropdown-item" href="#">선배의 조언</a></li>
                   <li><a class="dropdown-item" href="building.html">학교지도</a></li>
+                  <li><a class="dropdown-item" href="noticeboard.php">채팅방</a></li>
                   <li><hr class="dropdown-divider"></li>
                   <li><a class="dropdown-item" href="logout.php">로그아웃</a></li>
                   <li><a class="dropdown-item" href="logdel.php" style="color:red;">회원탈퇴</a></li>
@@ -203,7 +232,7 @@
             </ul>
             <form class="d-flex" role="search" method="get" action="search.php">
               <input class="form-control me-2" type="search" placeholder="검색하세요" aria-label="Search" name="search">
-              <button class="btn btn-outline-success" type="submit" id="go">GO!</button>
+              <button class="btn btn-outline-success btn-custom" type="submit" id="go">GO!</button>
             </form>
           </div>
         </div>
@@ -216,7 +245,7 @@
                 <td>
                     <p style="margin: 0; padding: 0; margin-top: 20px;"><li id="first_tip"> 수강신청 Tip</li></p>
                     <hr>
-                    <a href=""><div class="first_card"></div></a>
+                    <a href="ApplicationloginPage.html?num=<?=$num?>"><div class="first_card"></div></a>
                 </td>
             </tr>
         </div>
@@ -238,6 +267,24 @@
                 </td>
             </tr>
         </div>
+        <div class="four_card_base">
+            <tr>
+                <td>
+                    <p style="margin: 0; padding: 0;margin-top: 20px;"><li id="four_tip"> 학교지도</li></p>
+                    <hr>
+                    <a href="building.html"><div class="four_card"></div></a>
+                </td>
+            </tr>
+        </div>
+        <div class="five_card_base">
+            <tr>
+                <td>
+                    <p style="margin: 0; padding: 0;margin-top: 20px;"><li id="five_tip"> 채팅방</li></p>
+                    <hr>
+                    <a href="noticeboard.php"><div class="five_card"></div></a>
+                </td>
+            </tr>
+        </div>
       </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
@@ -245,6 +292,8 @@
             'img/학교사진1.png',
             'img/학교사진2.png',
             'img/학교사진3.png',
+            'img/학교사진4.png',
+            'img/학교사진5.png',
         ];
         let currentIndex = 0;
 
