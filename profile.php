fa3ba3a86e4c283c81e136e922e78db86a2d301e
hongjunpyo <0721hjp@naver.com>
2024-05-24 21:42:39 +0900

    fix: 회원정보 수정 페이지 요소 value값으로 수정 기입

--- profile.php
+++ profile.php
@@ -164,22 +164,22 @@
             <hr>
                 <div clasa = "email">
                 <label for="email"><b>이메일</b></label>
-                <input id="email" type="text" placeholder="<?=$email?>" name="email" required>
+                <input id="email" type="text" value="<?=$email?>" name="email" required>
                 </div>
                 <div class = "id">
                 <label for="num"><b>학번</b></label>
-                <input id="num" type="text" placeholder="<?=$num?>" name="num" required>
+                <input id="num" type="text" value="<?=$num?>" name="num" required>
                 </div>
                 <div class="password">
                 <label for="password"><b>비밀번호</b></label>
-                <input id="password" type="password" placeholder="<?=$password?>" name="password" required>
+                <input id="password" type="password" value="<?=$password?>" name="password" required>
                 </div>
                 <div class="name">
                 <label for="name"><b>닉네임</b></label>
-                <input id="name" type="text" placeholder="<?=$name?>" name="name" required>
+                <input id="name" type="text" value="<?=$name?>" name="name" required>
                 </div>    
                 <div class="clearfix">
-                    <button type="submit" class="signupbtn">확인</button>
+                    <button type="submit" class="signupbtn">수정하기</button>
                     <a href="main.php"><button type="button" class="cancelbtn">취소</button></a>
                     <a href="logdel.php"><button type="button" class="profilebtn">회원탈퇴</button></a>
                 </div>
