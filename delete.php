d99c32b7d0041dbde78141239138bf427f820194
hongjunpyo <0721hjp@naver.com>
2024-05-26 21:15:40 +0900

    파일 전부 삭제

--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once("conn.php"); 
-    session_start();
-    $id = $_GET['id'];
-    $conn->query("delete from noticeboard where id = '$id'");
-    echo "<script>location.href='noticeboard.php'</script>";
-?>
--- search.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $search =  $_GET['search'];
-?>
