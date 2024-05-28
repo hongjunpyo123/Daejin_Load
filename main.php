51a23f15720adaa10948a2b1150a8a4955851604
hongjunpyo <0721hjp@naver.com>
2024-05-28 22:58:04 +0900

    fix: main페이지 반응형 오류 수정 - 모바일 버전에서 지도 카드가 눌리지 않는 현상

--- main.php
+++ main.php
@@ -115,6 +115,8 @@
             }
             #low_bar{
                 opacity: 0;
+                display: none;
+                
             }
             #number{
             display: none;
